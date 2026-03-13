b27fc00cb7905ba0a9411fe3b98aa8e4abb96b58
Matheus Felipe <theusf40@hotmail.com>
2026-03-12 23:08:00 -0300

    Added auth

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 
 
 Route::get('/', [ChirpController::class, 'index']);
 
+Route::middleware('auth')->group(function () {
 Route::resource('chirps', ChirpController::class)
     ->only(['store', 'edit', 'update', 'destroy']);
+});
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+// Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
