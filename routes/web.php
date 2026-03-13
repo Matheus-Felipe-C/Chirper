7bfd1a7cd32fe3c16a32bc6d0ab3fd59acbc87dd
Matheus Felipe <theusf40@hotmail.com>
2026-03-12 22:56:20 -0300

    Added options to create new chirps, edit chirps and delete chirps

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
+
 Route::get('/', [ChirpController::class, 'index']);
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['store', 'edit', 'update', 'destroy']);
